238074bc689dc2d8f44eaf85c9af32705f778e2e
paetalung <paetalung@gmail.com>
2019-06-08 19:41:12 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace demo05.whileLoopCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int add = 0;
+            double total = 0.0;
+            double summa = 0.0;
+            Boolean check = true;
+
+            while (check)
+            {
+
+                Console.WriteLine("\nHow many rounds");
+                int round = int.Parse(Console.ReadLine());  // convert str 2 int
+
+                while (add < round)
+                {
+                    add++;
+                    Console.Write("Please enter your " + add + " value : ");
+                    summa = double.Parse(Console.ReadLine());
+                    total = total + summa;
+                }
+                Console.WriteLine("\nYour answer is : " + total + ".");
+
+                Console.Write("\nDo you want to continue ? (y/n) : ");
+                string get_check = Console.ReadLine();
+                
+
+                if (get_check == "y")
+                {
+                    check = true;
+                    round = 0;
+                    total = 0.0;
+                    summa = 0.0;
+                    add = 0;
+                }
+                else
+                {
+                    check = false;
+                }
+
+            }
+
+        }
+
+    }
+}
